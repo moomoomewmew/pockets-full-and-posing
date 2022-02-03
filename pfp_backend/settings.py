f43e1161fddde8fe2672e478b3615b960831c392
Katie Vermazani <katievermazani@tuta.io>
2022-02-03 08:43:39 -0700

    updated settings.py

--- pfp_backend/settings.py
+++ pfp_backend/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'pfp_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pfp',
+        'USER': 'pfpuser',
+        'PASSWORD': 'pfp',
+        'HOST': 'localhost'
     }
 }
 
