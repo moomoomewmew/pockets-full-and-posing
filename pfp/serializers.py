56982413176be5ec117231da69d9ef573341cd6c
Katie Vermazani <katievermazani@tuta.io>
2022-02-03 09:42:20 -0700

    backend done

---
+++ pfp/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Department, Item
+
+
+class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
+    items = serializers.HyperlinkedRelatedField(
+        view_name='item_detail',
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = Department
+        fields = ('id', 'name', 'image')
+
+
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
+    department = serializers.HyperlinkedRelatedField(
+        view_name='department_detail',
+        read_only=True)
+
+    class Meta:
+        model = Item
+        fields = ('id', 'department', 'description', 'image', 'price', 'quantity', 'size')
--- pfp/views.py
+++ pfp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import DepartmentSerializer, ItemSerializer
+from .models import Department, Item
 
-# Create your views here.
+
+class ItemList(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+
+class DepartmentList(generics.ListCreateAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class DepartmentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
