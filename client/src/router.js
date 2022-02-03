336ba8aa260c22136983d39c8425a56b1f038dc4
shelbylin13 <shelbylin13@gmail.com>
2022-02-03 11:16:08 -0600

    set up home

--- client/src/router.js
+++ client/src/router.js
@@ -1,7 +1,8 @@
 import VueRouter from 'vue-router';
 // import components
+import Home from './pages/Home';
 
-const routes = [];
+const routes = [{ path: '/', component: Home, name: 'Home' }];
 
 export default new VueRouter({
   routes,
