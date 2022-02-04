acba8c16ab1074150538bc734c98aada4f623702
shelbylin13 <shelbylin13@gmail.com>
2022-02-04 08:47:47 -0600

    ReadMe and About

--- client/src/pages/About.vue
+++ client/src/pages/About.vue
@@ -3,7 +3,7 @@
   <h2 class="about-header">About Pockets Full And Posing</h2>
   <div class ="about-info">
   <p>
-    Do you desire deluxe designs but find yourself with  demcimated doubloons? Wishing you could wear the wardrobe of the wealthy, but with a peasant price tag? Well, now you can! The makers of Creature Collab bring you Pockets Full And Posing: Royal Fashions For Regular Folk. Because everyone deserves to feel like nobility, no matter the count of coins in their coffers. 
+    Do you desire deluxe designs but find yourself with  decimated doubloons? Wishing you could wear the wardrobe of the wealthy, but with a peasant price tag? Well, now you can! The makers of Creature Collab bring you Pockets Full And Posing: Royal Fashions For Regular Folk. Because everyone deserves to feel like nobility, no matter the count of coins in their coffers. 
   </p>
   </div>
 </div>
